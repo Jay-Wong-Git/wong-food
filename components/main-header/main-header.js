fd4fd3d0ce0ca877d645bc333786ba4f9dc24d08
= <=>
2024-06-19 20:42:06 +0800

    complete foodies community page

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import logoImage from "@/assets/logo.png";
-import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
 import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -14,14 +15,13 @@ export default function MainHeader() {
           <Image src={logoImage} alt="A plate with food on it" priority />
           Wong Food
         </Link>
-
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>

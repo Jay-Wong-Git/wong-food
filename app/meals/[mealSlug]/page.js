baec523d3bffd2518d5e01ab269db6c01d8a9f2c
= <=>
2024-06-20 16:09:07 +0800

    add meal not found page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+  if (!meal) return notFound();
+
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
   return (

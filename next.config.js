4868419faffd6627fefba8444f6f1f29b323fa6d
= <=>
2024-06-21 09:44:36 +0800

    finish app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bsstpoekiaaigmjnfaua.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+  // output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
